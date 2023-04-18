99b4d18c3f2137960b54a2442b8b37fd7ba5cd86
Mutlu SENEM <info.mutlusenem@gmail.com>
2023-04-18 15:25:48 +0300

    Js Enes Bayram

---
+++ D-7 Yorum Satırı Kullanımı/app.js
@@ -0,0 +1,13 @@
+// Buraya bir yorum yazdım dikkat et.
+//? Bu bir bilgilendirme yorumudur
+//! Burası çok önemli.
+//TODO : Buranın şu şekilde yapılması gerekiyor
+//* Bu bir test mesajıdır
+
+/*
+Buraya bir yorum yazdım dikkat et.
+? Bu bir bilgilendirme yorumudur
+! Burası çok önemli.
+TODO : Buranın şu şekilde yapılması gerekiyor
+* Bu bir test mesajıdır
+*/
