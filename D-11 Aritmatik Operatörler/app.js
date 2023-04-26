dd234caca538a5fb719323fe42cad9c32fef9045
Mutlu SENEM <info.mutlusenem@gmail.com>
2023-04-26 18:51:31 +0300

    D-11 D-12 Update

---
+++ D-11 Aritmatik Operatörler/app.js
@@ -0,0 +1,101 @@
+//? ARİTMATİK OPERATÖRLER
+
+/*
+
+! = -----> Atama Operatörü ÖNEMLİ
++
+-
+*
+/
+%
+++
+--
+**
+
+*/
+
+
+//! = Atama operatörü:
+
+// let a = 5;
+
+// let isim = "Mutlu";
+
+// Yani burada eşitliğin sağ tarafındaki değeri al eşitliğin sol tarafındaki değişkenin içerisine koy demiş oluyoruz.
+
+//! + Artı operatörü matematikteki toplama işlemiyle aynı çalışır.
+
+// let not1 = 60;
+// let not2 = 70;
+// console.log(not1+not2);
+
+//Todo Ancak biz bir string ve bir numberi + operatöryle toplamak istersek bunları toplamayacak sadece yan yana yazacaktır
+//Todo Biz artı operatörünü birleştirmeler için de kullanabiliriz
+
+// let sayi1 = "5";
+// let sayi2 = 2;
+// console.log(sayi1+sayi2);
+
+
+// let not1 = 60;
+// let not2 = 70;
+// console.log("Notlarınızın Toplamı:" + (not1+not2)); // Burada parantez içindeki toplama işlemimizi önce yapıp String olarak yazdığımız yazı ile birleştirmiş olduk.
+
+//! - Eksi operatörü matematikteki çıkartma işlemi ile aynı çalışır
+
+// let not1 = 100;
+// let not2 = 70;
+// console.log(not1-not2);
+
+//! * Çarpı operatörü matematikteki çarpma işlemi ile aynı çalışır
+
+// let sayi1 = 5;
+// let sayi2 = 10;
+// console.log(sayi1*sayi2);
+
+//? Kısa not değişken ismi tanımlarken türkçe karakterler kullanmıyoruz (ü,ö,ş,İ,ğ)
+//? Değişken ismi tanımlarken boşluk bırakmıyoruz Örn; (let musteri No = 5;)
+//? Değişken isimleri sayı veya rakamla başlayamaz Örn; (let 1musteri No = 5;)
+//? Değişken isimleri oluştururken iki kelimeden oluşuyorsa ikinci kelimenin baş harfi büyük yazılır Örn; (let musteriNo = 5;)
+
+//! / Bölme operatörü de aynı şekilde matematikteki bölme işlemi ile aynı çalışır
+
+// let not1 = 100;
+// let not2 = 20;
+// console.log(not1/not2);
+
+//! % Mod alma operatörü = Bir sayıyı bir sayıya böldüğümüzde kalan fazlalık sayıyı bulmak için kullanılır 
+//! Örn: 10/3= Bu işlemde artık sayı olarak 1 kalır buradaki 1 i bulmak için kullandığımız operatör mod alma operatörüdür
+
+// console.log(10%3);
+// console.log(12%5);
+
+//! ++ Operatörü : Bir arttırmak için kullanılır
+
+// let a = 10;
+// a++; // Burada a değerini 1 arttırarak 11 yapmış olduk
+// a++; // Burada a değerini 1 arttırarak 12 yapmış olduk
+// console.log(a);
+
+//! -- Operatörü : Bir eksiltmek için kullanılır
+
+// let a = 10;
+// a--; // Burada a değerini 1 eksilterek 9 yapmış olduk
+// a--; // Burada a değerini 1 eksilterek 8 yapmış olduk
+// console.log(a);
+
+//! ** Çarpı-Çarpı operatörü : Bir sayının üstünü almak için kullanılır Örn: 2^3=8
+
+console.log(2**3);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ D-12 Atama Operatörü/app.js
@@ -0,0 +1,113 @@
+/* 
+
+ATAMA OPERATÖRLERİ
+
+=
+==
+===
++=
+*=
+/=
+%=
+**=
+
+*/
+
+//! = : Atama Operatörü
+// Eşitliğin sağ tarafındaki değeri alıp eşitliğin sol tarafındaki değişkenin içerisine koyar
+
+// let a = 5; 
+
+//! == : Eşittir operatörü
+// Matematikteki eşittir işleminin aynısıdır.
+
+// console.log(5==5);
+
+// let a = 5;
+// let b = 10;
+// let sonuc = a+b;
+// console.log(sonuc ==10);
+
+
+// console.log(3=="3"); // Burada veri tiplerinden önemsiz içerdeki değerler eşitse eşittir operatöründe true sonucunu alırız.
+
+//! === : Üç Eşittir operatörü
+// Hem tipleri hem de değerlerinin eşit olup olmadığına bakar
+
+// console.log(5===5); // Burada true sonucu alırız
+// console.log(5==="5"); // Burada false sonucu alırız
+
+//! += : Artı Eşittir Operatörü
+// Biz bir değişkene verdiğimiz değeri toplama işlemiyle toplayıp değeri değiştirmek istediğimizde bu işlem için kullandığımız
+// ve işimizi kolaylaştıracak operatördür
+
+//Örn;
+
+    // let sayi = 4;
+    // sayi = sayi+2; //Normalde sayi ismindeki değişkenimizin değerini 6 yapmak için bu şekilde yazabilirdik
+    // console.log(sayi);
+
+// += Operatörü kullanarak bu işlemi daha basit şu şekilde yapabiliriz
+
+// let sayi = 4;
+// sayi+=2;
+// console.log(sayi);
+
+// Yani burada + operatörüyle 4' e 2 ekledik ve eşittir operatörü ile çıkan sonucu tekrar sayi değişkenimize atamış olduk.
+
+//! -= : Eksi Eşittir Operatörü
+// Artı eşittir ile aynı çalışır. Eksi operatörüyle değişkenimizdeki değerimizden bir sayıyı çıkartır ve eşittir operatörü ile
+// çıkan sonucu tekrar değişkene atar
+
+// Örn;
+
+// let sayi = 5
+// sayi-=3;
+// console.log(sayi);
+
+//! *= : Çarpı Eşittir Operatörü
+// Artı eşittir ve Eksi eşittir ile aynı çalışır. Çarpı operatörüyle değişkenimizdeki değerimizi yeni verdiğimiz değer ile çarpar
+// ve çıkan sonucu eşittir operatörü sayesinde değişkenimize tekrar atar
+
+// Örn;
+
+// let sayi = 5;
+// sayi*=3;
+// console.log(sayi); //Burada sayi değişkenimizin değerini 15 olarak görmüş oluruz
+
+//! /= : Bölü Eşittir Operatörü
+// Çarpı eşittirle aynı çalışır. Bölü operatörüyle değişkenimizdeki değerimizi yeni verdiğimiz değer ile böler ve
+// çıkan sonucu eşittir operatörü sayesinde değişkenimize tekrar atar
+
+// let sayi = 15;
+// sayi/=3;
+// console.log(sayi); //Burada sayi değişkenimizin değerini 15/3 Yani 5 olarak görürüz
+
+//! %= : Mod Eşittir Operatörü
+// Bir bölme işleminden kalan artık sayıyı değişkenimize atamak için kullanılır
+
+// Örn;
+
+// let sayi = 12;
+// sayi%=5;
+// console.log(sayi); //Burada 12 yi 5 e böldük kalan artık sayı olan 2 yi değişkenimize tanımlamış olduk
+
+//! **= : Çarpı Çarpı Eşittir Operatörü
+// Değişkenimize verdiğimiz değerin üstünü alıp aldığımız sonucu değişkenimize tekrar atamak için kullanılır.
+
+//Örn;
+
+// let sayi = 4;
+// sayi**=2;
+// console.log(sayi); //Burada sayi değişkenimizin karşılığı olan 4 değerimize 4 üzeri 2 (4^2) işlemini 
+//yapmış oluyoruz ve çıkan sonucu tekrar sayi değişkenimize atamış oluyoruz (4^2=16)
+
+
+
+
+
+
+
+
+
+
