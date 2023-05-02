7fe120641a647e4c1d65f809186cd78ed7e11b10
Mutlu SENEM <info.mutlusenem@gmail.com>
2023-05-02 21:38:00 +0300

    D-14 Update

---
+++ D-14 Karşılaştırma Operatörleri/app.js
@@ -0,0 +1,75 @@
+//? KARŞILAŞTIRMA OPERATÖRLERİ
+
+/*
+
+!= : Eşit değil mi
+>  : Büyük mü
+<  : Küçük mü
+>= : Büyük veya eşit mi
+<= : Küçük veya eşit mi
+
+*/
+
+//! != : Eşit değil mi
+// Bir değer diğer değere eşit değilse true sonucunu aldığımız operatördür. Eşittir operatörüyle tam ters çalışır. Örn;
+
+// console.log(5!=3);
+
+// Burada 5-3'ten büyük olduğu ve eşit olmadığı için true sonucunu alırız
+
+// console.log(5!=5);
+
+// Burada ise 5-5'e eşit olduğu için eşit değildir operatörümüz bize false sonucunu verir
+
+// let yas = 23;
+
+// console.log(yas!=18); // Burada yaş değerimiz 18' e eşit olmadığı için true sonucunu alırız.
+
+//! > : Büyüktür operatörü
+
+// Sorduğumuz değer diğer değerden büyükse true sonucunu aldığımız operatördür. Örn;
+
+// console.log(15>12);
+
+//! < : Küçüktür operatörü
+
+// Büyüktür operatörünün tam tersi mantıkla çalışır sorduğumuz değer küçükse true sonucunu alırız. Örn;
+
+// console.log(3<12);
+
+//! >= : Büyük veya Eşit mi
+
+// Sorguladığımız değer diğerinden büyük veya eşitse true sonucunu alırız. Örn;
+
+// console.log(12>=12); //Burada eşit olduğu için True sonucunu alırız
+
+// console.log(12>=8);  //Burada 12-8'den büyük olduğu için True sonucunu alırız
+
+// console.log(12>=22); // Burada 12 büyük veya eşit olmadığı için false sonucunu alırız
+
+//! <= : Küçük veya Eşit mi
+
+// Sorguladığımız değer diğerinden küçük veya eşitse true sonucunu alırız. Örn;
+
+console.log(8<=8); // Burada eşit olduğu için True sonucunu alırız
+
+console.log(3<=8); // Burada 3-8'den küçük olduğu için True sonucunu alırız
+
+console.log(13<=8); // Burada 13-8'den küçük veya eşit olmadığı için false sonucunu alırız.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
