aeb8d23b2acc55b6c50257334be90aa916f5ece4
Mutlu SENEM <info.mutlusenem@gmail.com>
2023-05-27 16:25:27 +0300

    D-19 Update

---
+++ D-19 If-Else If-Else Yapısı/app.js
@@ -0,0 +1,28 @@
+//? If-Else If-Else Yapısı
+
+/*
+
+if(koşul){
+    kodlar
+}
+
+else if(koşul){
+    kodlar
+}
+
+else if(koşul){
+    kodlar
+}
+
+else(koşul){
+    kodlar
+}
+
+*/
+
+//! Birden çok ihtimalin ve koşulun olduğu yerlerde else if kullanılıyor.
+/*
+Derleyici önce if koşuluna bakıyor koşul true ise if kodları çalışıyor.
+Eğer if koşulu sağlanmıyorsa else if'lere bakıyor koşul true ise else if kodları çalışıyor.
+Eğer else if koşulları da sağlanmıyorsa else kodları çalışıyor. Yani hiç biri koşulu sağlamazsa else çalışıyor.
+ */
