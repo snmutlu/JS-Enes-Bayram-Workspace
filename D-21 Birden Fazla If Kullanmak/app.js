70595886e916f15feb298fb5d20054f2d8e7e47f
Mutlu SENEM <info.mutlusenem@gmail.com>
2023-06-12 17:38:31 +0300

    D-21 Update

---
+++ D-21 Birden Fazla If Kullanmak/app.js
@@ -0,0 +1,23 @@
+//? Birden Fazla If Kullanmak
+
+/* 
+ if(koşul){
+ Koşul true ise buradaki kodlar çalışır. False ise çalışmaz.
+ }
+
+if(koşul){
+ Koşul true ise buradaki kodlar çalışır. False ise çalışmaz.
+ }
+
+ if(koşul){
+ Koşul true ise buradaki kodlar çalışır. False ise çalışmaz.
+ }
+
+ Çoklu if yapısı
+ 
+ Bir koşul yapısı sadece iflerden oluşuyorsa ve herhangi bir if için koşul true olarak dönüyorsa o if'in kodlarını çalıştırır.
+ Bütün if'lere ayrı ayrı bakar ve koşulun true olduğu bütün if'ler çalışır. Eğer if koşulu false ise çalışmaz.
+ Çoklu if yapılarında bütün if'ler birbirinden ayrı ve izole şekilde çalışır.
+
+
+*/
