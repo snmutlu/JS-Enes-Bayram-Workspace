b0c623ed0c92788dbb6fd8c2f5159a826abca718
Mutlu SENEM <info.mutlusenem@gmail.com>
2023-06-25 16:28:18 +0300

    D-26 Update

--- D-26 Haftanın Günleri Uyg-Switch-Case/app.js
+++ D-26 Haftanın Günleri Uyg-Switch-Case/app.js
@@ -2,9 +2,10 @@
 
 const yeniSatir = "\r\n"
 
-let sayi = prompt("Lütfen 1 ile 5 arasında bir sayı giriniz")
+let sayi = prompt("Lütfen 1 ile 5 arasında bir sayı giriniz");
 
-
+//Burada kullanıcıdan bir sayı değeri alıyoruz ve sayı değişkenimize atıyoruz.
+//Aldığımız sayı değeri hangi case'e eşitse o case'deki kodlar çalışıyor.
 switch(sayi){ 
 
     case "1":
@@ -23,9 +24,10 @@ switch(sayi){
     case "4":
         console.log("Girilen sayı:"+sayi);
         break;
-        
+
     case "5":
         console.log("Girilen sayı:"+sayi);
+        break;
 
     default:
         alert("Girilen sayı 1 ile 5 arasında olmalıdır!"+yeniSatir
@@ -33,6 +35,54 @@ switch(sayi){
         break;
 
 }
+
+
+//------------------------------------------
+
+// const yeniSatir = "\r\n";
+
+// let metin = "1-Pazartesi" + yeniSatir
+//     + "2-Salı" + yeniSatir
+//     + "3-Çarşamba" + yeniSatir
+//     + "4-Perşembe" + yeniSatir
+//     + "5-Cuma" + yeniSatir
+//     + "6-Cumartesi" + yeniSatir
+//     + "7-Pazar" + yeniSatir
+//     +"Lütfen bir seçim yapınız:";
+
+// let deger = prompt(metin)
+// switch (deger) {
+//     case "1":
+//         console.log("Pazartesi günü");
+//         break;
+
+//     case "2":
+//         console.log("Salı günü");
+//         break;
+
+//     case "3":
+//         console.log("Çarşamba günü");
+//         break;
+
+//     case "4":
+//         console.log("Perşembe günü");
+//         break;
+
+//     case "5":
+//         console.log("Cuma günü");
+//         break;
+
+//     case "6":
+//         console.log("Cumartesi günü");
+//         break;
+
+//     case "7":
+//         console.log("Pazar günü");
+//         break;
+
+//         default:
+//             console.log("Lütfen geçerli bir değer giriniz!");
+// }
 
 
 
