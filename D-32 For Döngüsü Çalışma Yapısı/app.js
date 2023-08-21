14d269bf2c7237e24f9ca020e85c03887a502c7c
Mutlu SENEM <info.mutlusenem@gmail.com>
2023-08-21 16:50:55 +0300

    D-32 Upload

---
+++ D-32 For Döngüsü Çalışma Yapısı/app.js
@@ -0,0 +1,21 @@
+//? For Döngüsü Çalışma Yapısı
+
+/*
+For döngüsü 3 bölümden oluşuyor.
+1-Değişkeni tanımladığımız yer
+2-Koşul
+3-Arttırma veya Azaltma
+
+Örn;
+
+for(değişken; koşul; arttırma-azaltma){
+    ///kodlar
+}
+ */
+
+for(let i=1; i<=10; i++){
+    console.log(i);
+    alert(i);
+}
+
+/*Burada derleyici önce değişkeni oluşturuyor sonra koşulu kontrol ediyor ve koşul true olarak dönüş sağlıyorsa kıvırcık parantezler içerisindeki kodlarımızı çalıştırıyor. Ve sonradan geri dönüp arttırma işlemimizi gerçekleştiriyor. */
