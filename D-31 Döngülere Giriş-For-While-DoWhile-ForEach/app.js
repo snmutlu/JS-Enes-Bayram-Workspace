08adfead6179ccee0eca60ceb1eccf02885e13ec
Mutlu SENEM <info.mutlusenem@gmail.com>
2023-08-21 16:33:35 +0300

    D-31 Upload

---
+++ D-31 Döngülere Giriş-For-While-DoWhile-ForEach/app.js
@@ -0,0 +1,19 @@
+//? Döngülere Giriş
+
+//! For-While-(Do-While)-ForEach
+
+/*
+
+Döngüler = Loops
+Bir şeylerin sürekli tekrarlanmasını istediğimiz yerlerde döngüleri kullanıyoruz.
+
+1-For
+2-While
+3-Do-While
+4-ForEach
+
+Bu döngülerin yapısal farklılıkları var ve farklı alanlarda kullanılabiliyorlar.
+En sık kullanılanlar For ve While döngüleri oluyor.
+ForEach'i de dizilerde kullanıyoruz.
+
+*/
