014b7869e66508dffcc6778c98bd44cc87e5bb04
Mutlu SENEM <info.mutlusenem@gmail.com>
2023-08-24 17:24:46 +0300

    D-34 Upload

---
+++ D-34 While Döngüsü Çalışma Yapısı/app.js
@@ -0,0 +1,30 @@
+//? While Döngüsü Çalışma Yapısı
+
+/*
+
+while(koşul){
+    ///Kodlar
+
+
+    ///Sayacın değerini atarız
+} 
+
+*/
+
+/*
+!While döngüsünde parantez içerisindeki koşul true olarak dönüyorsa, kıvırcık parantezlerin içerisindeki kodlar sayaç değeri kadar tekrarlanır ve çalıştırılır.
+*Koşul false dönüyorsa while döngüsü çalışmaz ve derleyici while döngüsüne girmez, alt satırlardaki kodlar okunmaya devam eder
+ */
+
+/*
+
+Örn;
+not=75
+while(not>50){
+
+    ///Kodlar
+
+    ///Sayaç değeri
+}
+
+*/
