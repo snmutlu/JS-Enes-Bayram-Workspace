f91ecd1de0316cc8ba944badb0d5126a02eadb89
Mutlu SENEM <info.mutlusenem@gmail.com>
2023-08-26 23:10:37 +0300

    up

--- D-36 Do-While Döngüsü Çalışma Yapısı/app.js
+++ D-36 Do-While Döngüsü Çalışma Yapısı/app.js
@@ -11,5 +11,5 @@ while koşulu true olsa da false olsa da 1 kere çalışır ve derleyici yukarı
 koşul false olursa döngü tekrar çalışmaz.
 
 Yani kısacası Do-While 'ın While 'dan farkı do kısmındaki kodların derleyici tarafından her durumda koşul sağlansa da
-sağlanmasa da do kısmındaki kodlarımızın çalışmasıdır.
+sağlanmasa da do kısmındaki kodlarımızın çalışmasıdır..
 */
