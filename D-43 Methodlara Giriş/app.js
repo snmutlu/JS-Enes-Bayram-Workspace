c1082721c66da618536f8d053d78c8fdf450b818
Mutlu SENEM <info.mutlusenem@gmail.com>
2023-09-03 17:25:05 +0300

    D-43-44 Upload

---
+++ D-43 Methodlara Giriş/app.js
@@ -0,0 +1,10 @@
+//? Methodlara Giriş (function)
+
+/*
+
+Methodların kullanım amacı projelerdeki kodların modüler modüler bölünmesi içindir.
+Örneğin bir proje içerisinde yapılan bütün işlemleri proje içerisine yazmak yerine bunları methodlar olarak
+parça parça kodlayıp projede birleştirebiliriz. Bu sayede ihtiyacımız olan her şeyi tekrar tekrar yazmak yerine
+yazılmış bir methodu tekrar tekrar çağırarak bu işlemleri gerçekleştirebiliyoruz.
+
+*/
---
+++ D-44 Parametresiz Method Oluşturma/app.js
@@ -0,0 +1,31 @@
+//? Parametresiz Method Oluşturma
+
+/* Parametresiz ve geriye değer döndürmeyen method tanımlamak. */
+
+/*
+
+function yazdir(){
+    kodlar yazılacak
+}
+
+*/
+
+// function yazdir(){
+//     debugger;
+//     console.log("Mutlu");
+// }
+
+// debugger;
+// yazdir();
+// yazdir();
+
+
+
+// Örneğin toplama işlemi yaptıracağımız bir method oluşturalım
+
+function topla(){
+    console.log(5+7);
+}
+topla();
+
+// Functionları önce oluşturuyoruz ve sonrasında kullanmak istediğimiz yerde bunları tekrar tekrar çağırabiliriz.
