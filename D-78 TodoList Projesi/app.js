832ad72289c890ef669982ba3f1acd12c7418939
Mutlu SENEM <info.mutlusenem@gmail.com>
2023-10-14 22:27:11 +0300

    Update

--- D-78 TodoList Projesi/app.js
+++ D-78 TodoList Projesi/app.js
@@ -10,66 +10,107 @@ const clearButton = document.querySelector("#clearButton");
 let todos = [];
 
 runEvents();
-function runEvents(){
-    form.addEventListener("submit",addTodo);
+function runEvents() {
+    form.addEventListener("submit", addTodo);
+    document.addEventListener("DOMContentLoaded", pageLoaded);
+    secondCardBody.addEventListener("click",removeTodoToUI);
 }
 
-function addTodo(e){
-    const inputText = addInput.value.trim();
-    if(inputText==null || inputText==""){
-        alert("Lütfen bir değer giriniz!");
-        addInput.value="";
+function pageLoaded() {
+    checkTodosFromStorage();
+    todos.forEach(function (todo) {
+        addTodoToUI(todo);
+    });
+
+}
+
+function removeTodoToUI(e){
+    if(e.target.className=="fa fa-remove"){
+        const todo = e.target.parentElement.parentElement;
+        todo.remove();
+        showAlert("warning","Todo Başarıyla Silindi");
     }
-    else{
+}
+
+function addTodo(e) {
+    const inputText = addInput.value.trim();
+    if (inputText == null || inputText == "") {
+        addInput.value = "";
+        showAlert("danger", "Lütfen Bir Değer Giriniz");
+
+    }
+    else {
         // Arayüze ekleme
         addTodoToUI(inputText);
         addTodoToStorage(inputText);
-        
+        showAlert("success", "Todo Başarıyla Eklendi");
+
     }
     // Storage'a ekleme
     e.preventDefault();
 }
 
-function addTodoToUI(newTodo){
-    
+function addTodoToUI(newTodo) {
+
     // <li class="list-group-item d-flex justify-content-between">Todo 1
     //     <a href="#" class="delete-item">
     //         <i class="fa fa-remove"></i>
     //     </a>
     // </li>
+    // * Yukarıdaki yapıyı aşağıda oluşturuyoruz.
+
 
     const li = document.createElement("li");
-    li.className="list-group-item d-flex justify-content-between";
-    li.textContent=newTodo;
+    li.className = "list-group-item d-flex justify-content-between";
+    li.textContent = newTodo;
 
     const a = document.createElement("a");
-    a.href="#";
-    a.className="delete-item";
+    a.href = "#";
+    a.className = "delete-item";
 
     const i = document.createElement("i");
-    i.className="fa fa-remove";
-    
+    i.className = "fa fa-remove";
+
 
     a.appendChild(i);
     li.appendChild(a);
     todoList.appendChild(li);
 
-    addInput.value="";
+    addInput.value = "";
 
 
 }
 
-function addTodoToStorage(newTodo){
+function addTodoToStorage(newTodo) {
     checkTodosFromStorage();
     todos.push(newTodo);
-    localStorage.setItem("keyTodos",JSON.stringify(todos));
+    localStorage.setItem("keyTodos", JSON.stringify(todos));
 }
 
-function checkTodosFromStorage(){
-    if(localStorage.getItem("keyTodos")===null){
+function checkTodosFromStorage() {
+    if (localStorage.getItem("keyTodos") === null) {
         todos = [];
     }
-    else{
+    else {
         todos = JSON.parse(localStorage.getItem("keyTodos"));
     }
+}
+
+function showAlert(type, message) {
+
+    //     <div class="alert alert-success" role="alert">
+    //   This is a success alert—check it out!
+    // </div>
+    // * Yukarıdaki yapıyı aşağıda oluşturuyoruz.
+
+    const div = document.createElement("div");
+    // div.className="alert alert-"+type;
+    //* Yukarıdaki kodun aynısı litirel template kullanım örneği
+    div.className = `alert alert-${type}`;
+    div.textContent = message;
+    firstCardBody.appendChild(div);
+
+    setTimeout(function () { //* Bu fonksiyon oluşturulan divi kaldırmak için kullanıldı
+        div.remove();
+    }, 1700) //* Buradaki virgülden sonraki değer timeout milisaniyesi.
 }
