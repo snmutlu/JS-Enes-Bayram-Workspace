469f0341b469f72f6c4381e487481367e3052c3e
Mutlu SENEM <info.mutlusenem@gmail.com>
2023-12-04 22:14:40 +0300

    D-87 Upload

---
+++ D-87 EcmaScript Nedir (ES6)/app.js
@@ -0,0 +1,7 @@
+//? EcmaScript (ES6) Nedir?
+
+/* EcmaScript bir dil değildir, JavaScript üzerine oluşturulmuş bir yazım standartıdır.
+Ecmascript ile yazdığımız kodlar bütün tarayıcılarda uyumlu bir şekilde çalışmaktadır.
+Bu yüzden yazdığımız kodları EcmaScript standartlarına uygun bir şekilde yazıyoruz.
+EcmaScript'in bir çok versiyonu mevcut. Örn: ES1-ES2-ES3-ES4-ES5-ES6
+*/
