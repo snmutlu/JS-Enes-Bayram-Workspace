a660f36bda6d77fb6caaa6cd546fd3baea666dd3
Mutlu SENEM <info.mutlusenem@gmail.com>
2023-12-22 01:15:23 +0300

    D-95 Upload

---
+++ D-95 OOP(Nesne Yönelimli Programlama)/app.js
@@ -0,0 +1,12 @@
+//? OOP Nedir?
+
+/*
+OOP Nesne Yönelimli Programlama demektir. OOP sayesinde js
+backend kısmında da kullanılabilir oldu. Basit seviyede nesne
+yönelimli programlama yapabiliyoruz. Bizler oop sayesinde
+gerçek hayattaki objeleri,nesneleri programlama sistemine anlatmış oluyoruz.
+Yani nesneleri soyutlaştırabiliyoruz.
+Örn; Bir araba var ve biz bu arabanın özelliklerini programlama
+diline anlatmış oluyoruz. Bizler bu şekilde binlerce nesneyi
+programlama diline entegre edebiliyoruz.
+*/
