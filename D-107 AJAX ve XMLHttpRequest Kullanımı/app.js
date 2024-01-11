5fa7f4231119f2b536ceea31d0ae0033a6d6db8f
Mutlu SENEM <info.mutlusenem@gmail.com>
2024-01-11 19:59:59 +0300

    D-107 Update

--- D-107 AJAX ve XMLHttpRequest Kullanımı/app.js
+++ D-107 AJAX ve XMLHttpRequest Kullanımı/app.js
@@ -1,2 +1,45 @@
 //? AJAX Nedir? - AJAX Kullanımı
 
+
+function prepareURL(url,id){
+    if(id==null){
+        return url;
+    }
+    return `${url}?postId=${id}`
+    
+}
+
+
+
+function getComments(url,id){
+    let newURL = prepareURL(url,id);
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange",()=>{
+        if(xhr.readyState===4 && xhr.status===200){
+            console.log(JSON.parse(xhr.responseText));
+        }
+    })
+
+    xhr.open("GET",newURL);
+    xhr.send();
+}
+getComments("https://jsonplaceholder.typicode.com/comments",1)
+
+
+//---------------------------------------------------------------//
+//*Başka bir örnek:
+
+function getData(url){
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange",()=>{
+        if(xhr.readyState===4 && xhr.status===200){
+            console.log(JSON.parse(xhr.responseText));
+        }
+    })
+
+xhr.open("GET",url);
+xhr.send();
+}
+
+
+getData("https://jsonplaceholder.typicode.com/users")
